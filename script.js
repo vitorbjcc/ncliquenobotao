a725d9fb451682be7808529e43f2f21dcf7e6721
vitorbjcc <101301985+vitorbjcc@users.noreply.github.com>
2022-12-05 17:52:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const Thevideo = document.createElement('video');
 
+const btn = document.querySelector('button#btn');
+
 function unpause() {
 
     Thevideo.play();
@@ -9,6 +11,14 @@ function unpause() {
 function video() {
 
     const divRes = document.querySelector('div#res');
+    
+    btn.style.width = '0px';
+    
+    btn.style.height = '0px';
+    
+    btn.textContent = '';
+    
+    btn.style.
 
     Thevideo.setAttribute('src', 'video.mp4');
 
